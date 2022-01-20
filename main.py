2c35104e94f707386c6dab7580cb1d4fa8df6f43
Suvid Datta <suvid.datta@gmail.com>
2022-01-20 18:11:03 +0530

    ide les go

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from fileinput import filename
 from tkinter import *
 from tkinter import filedialog
 import json
@@ -33,6 +34,8 @@ root.title(title)
 root.minsize(height=250, width=350)
 root.maxsize(height=1000, width=1000)
 
+
+
 # adding scrollbar
 scrollbar = Scrollbar(root)
 
@@ -60,13 +63,14 @@ cdg = ic.ColorDelegator()
 cdg.prog = re.compile(r'\b(?P<MYGROUP>tkinter)\b|' + ic.make_pat(), re.S)
 cdg.idprog = re.compile(r'\s+(\w+)', re.S)
 cdg.tagdefs['MYGROUP'] = {'foreground': '#7F7F7F', 'background': 'background'}
-cdg.tagdefs['COMMENT'] = {'foreground': '#FF0000', 'background': 'background'}
+cdg.tagdefs['COMMENT'] = {'foreground': 'green', 'background': 'background'}
 cdg.tagdefs['KEYWORD'] = {'foreground': '#007F00', 'background': 'background'}
 cdg.tagdefs['BUILTIN'] = {'foreground': '#7F7F00', 'background': 'background'}
-cdg.tagdefs['STRING'] = {'foreground': 'purple', 'background': 'background'}
+cdg.tagdefs['STRING'] = {'foreground': 'orange', 'background': 'background'}
 cdg.tagdefs['DEFINITION'] = {'foreground': '#007F7F', 'background': 'background'}
 cdg.tagdefs['FUNCTION'] = {'foreground': 'blue', 'background': 'background'}
-cdg.tagdefs['VARIABLES'] = {'foreground': 'green', 'background': 'background'}
+cdg.tagdefs['VARIABLE'] = {'foreground': 'red', 'background': 'background'}
+cdg.tagdefs['CLASS'] = {'foreground': 'blue', 'background': 'background'}
 
 
 # configuring the scrollbar
@@ -75,7 +79,11 @@ scrollbar.config(command=text_info.yview)
 
 
 
-def load_file():
+
+
+
+
+def load_file(*args):
     global file_name
     file_name = filedialog.askopenfilename()
     text_info.delete(1.0,END)
@@ -91,6 +99,26 @@ def load_file():
     else:
         pass
 
+def load_certian_file(file_name):
+    try:
+        text_info.delete(1.0,END)
+        text_info.insert(END, open(file_name).read())
+        with open('cache/openedfiles.txt','a') as f:
+            f.write(file_name)
+            f.write('\n')
+            f.close()
+        # if extension is .py turn syntax on
+        if file_name.endswith(".py"):
+            ip.Percolator(text_info).insertfilter(cdg)
+        else:
+            pass
+    except IsADirectoryError:
+        # insert the names of the files in the directory into the folder widget under the directory
+        # find the directory's location in the folder widget
+        # insert the names of the files in the directory into the folder widget under the directory
+        dir_location = folder.get(folder.curselection())
+        for file in os.listdir(file_name):
+            folder.insert(dir_location, file)
 
 def save_file():
     file = open(file_name, "w")
@@ -150,18 +178,33 @@ def command_line(command):
             f.close()
         os.system("rm cache/answer.txt")
 
+# add a folder widget to show the files in the current directory
+folder = Listbox(root, height=30, width=50)
+folder.configure(font=font, bg=background, fg=textcolor)
+folder.pack(side=LEFT, fill=X)
+for file in os.listdir():
+    filenamex = file
+    folder.insert(END, filenamex)
+    
+    
+    
+# if a file is selected in the folder widget, load it
+folder.bind("<<ListboxSelect>>", lambda event: load_certian_file(folder.get(folder.curselection())))
+
 
     
                 
 #Add a basic commandline to open settings and cached stuff 
 COMMANDBOX = Entry(root)
 COMMANDBOX.insert(END, "Write a command here")
+COMMANDBOX.configure(font=font, bg=background, fg=textcolor)
 COMMANDBOX.pack(side=BOTTOM, fill=X)
 #add a widget that shows the commands given in COMMANDBOX
 COMMANDBOX.bind("<Return>", lambda event: command_line(COMMANDBOX.get()))
 
 REPLYBOX = Text(root, height=30, width=50)
 REPLYBOX.insert(END, "CrispyCommandLine")
+REPLYBOX.configure(font=font, bg=background, fg=textcolor)
 REPLYBOX.pack(side=BOTTOM, fill=X)
 
 
@@ -176,6 +219,23 @@ file_menu.add_command(label="Open", command=load_file)
 file_menu.add_command(label="Save", command=save_file)
 file_menu.add_command(label="Save As", command=save_as)
 file_menu.add_command(label="New", command=new_file)
+
+#open the last file in cache/openedfiles.txt
+with open('cache/openedfiles.txt') as f:
+    try:
+        # load the last file in cache/openedfiles.txt
+            file_name = f.readlines()[-1]
+            file_name = file_name.strip('\n')
+            text_info.delete(1.0,END)
+            text_info.insert(END, open(file_name).read())
+            # if extension is .py turn syntax on
+            if file_name.endswith(".py"):
+                ip.Percolator(text_info).insertfilter(cdg)
+            else:
+                pass
+            f.close()
+    except IndexError:
+        pass
 
 
 root.mainloop()
